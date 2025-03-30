510f80c9b307db41f8e13592f4c017cfa8b99247
Hassan Raza <167288072+HassanRaza2020@users.noreply.github.com>
2025-03-30 23:35:22 +0500

    dashboard updated

--- src/app/auth/dashboard/dashboard.component.ts
+++ src/app/auth/dashboard/dashboard.component.ts
@@ -14,7 +14,7 @@ export class DashboardComponent implements OnInit {
 
   public chart: any;
   categoryCount=[];
-  categoryId = [];
+  category = [];
   totalCount = [];
 
   ngOnInit(): void {
@@ -23,26 +23,43 @@ export class DashboardComponent implements OnInit {
     this.apiService.getDashboard().subscribe(response=>{     
     this.categoryCount = response;
     for(let item of this.categoryCount){
-      this.categoryId.push(item.content);
+      this.category.push(item.content);
       this.totalCount.push(item.total);
-   
+
     }
-    console.log("Total:", this.totalCount);
-   
+    this.createChart();
+
+   console.log("category:", this.category);
+   console.log("Total:", this.totalCount);
+
+    
   })
 
   }
 
   createChart() {
+
+   if(this.chart){
+    this.chart.destroy();
+   }
+
     this.chart = new Chart("MyChart", {
       type: 'bar',
       data: {
-        labels: [this.categoryId],
+        labels: this.category,
         datasets: [
           {
-            label: 'Questions',
-            data: [12,23,223,33,44,44,44,44,,44,44],
-            backgroundColor: 'red'
+            label: 'Category',
+            data: this.totalCount,
+            backgroundColor: [
+              'rgba(114, 59, 71, 0.2)',
+              'rgba(248, 163, 77, 0.2)',
+              'rgba(255, 205, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(201, 203, 207, 0.2)'
+            ],
           },
         ]
       },
